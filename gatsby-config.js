c17e470f9f93468cdc6efa9eeafde10d80bcf7eb
dtjokroa <dtjokroa@gmail.com>
2020-12-02 22:01:27 +0700

    Changed metadata title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Pivot BTM Case Competition',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Pivot is a BTM Case Competition based in Vancouver, BC.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
