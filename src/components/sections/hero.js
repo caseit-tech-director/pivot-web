3015baf12fb85f20c42ad6ef29b251a4e41b9393
Alvin Leung <alvinleung2009@gmail.com>
2021-01-15 13:18:01 -0800

    Fixing case issue (part3)

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./Hero.scss";
+import "./hero.scss";
 import { Link } from "gatsby";
 
 const hero = ({ pivotBeginDate, pivotEndDate, daysLeft, applicationLink }) => (
