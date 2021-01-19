f27b931ff10a7c356d82e155da4154bbd4764344
Alvin Leung <alvinleung2009@gmail.com>
2021-01-19 04:35:10 -0800

    SSR fix attempt 4

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -101,7 +101,7 @@ const NavBar = () => {
           {MenuItems.map(({ title, url }, index) => {
             const linkClassName = (() => {
               // for server side rendering
-              if (window !== null) {
+              if (typeof window !== "undefined") {
                 return window.location.hash === url
                   ? "nav-link nav-link--current"
                   : "nav-link";
