9a4a94a9b8c2a6476e6adaae4be2f1e3809887fa
Alvin Leung <alvinleung2009@gmail.com>
2021-01-18 19:36:51 -0800

    Refractor alumni list

--- src/components/AlumniDisplay/AlumniDisplay.js
+++ src/components/AlumniDisplay/AlumniDisplay.js
@@ -1,24 +1,8 @@
 import React from "react";
 import "./AlumniDisplay.scss";
+import alumniList from "../../data/AlumniGuestList";
 
 const AlumniDisplay = ({ className }) => {
-  const alumniList = [
-    {
-      name: "Brian Yan Muk",
-      position: "Director of Education, Nextech AR Solution",
-      image: "img/headshot-brian.jpg",
-    },
-    {
-      name: "Michelle Chen",
-      position: "Associate Product Manager, Electronic Arts",
-      image: "img/headshot-michelle-chen.jpg",
-    },
-    {
-      name: "Anthony Siu",
-      position: "Project Manager, GAP",
-      image: "img/headshot-anthony-siu.jpg",
-    },
-  ];
   return (
     <div className={`alumni-profile-container ${className}`}>
       {alumniList.map((val) => {
---
+++ src/data/AlumniGuestList.js
@@ -0,0 +1,19 @@
+const alumniList = [
+  {
+    name: "Brian Yan Muk",
+    position: "Director of Education, Nextech AR Solution",
+    image: "img/headshot-brian.jpg",
+  },
+  {
+    name: "Michelle Chen",
+    position: "Associate Product Manager, Electronic Arts",
+    image: "img/headshot-michelle-chen.jpg",
+  },
+  {
+    name: "Anthony Siu",
+    position: "Project Manager, GAP",
+    image: "img/headshot-anthony-siu.jpg",
+  },
+];
+
+export default alumniList;
